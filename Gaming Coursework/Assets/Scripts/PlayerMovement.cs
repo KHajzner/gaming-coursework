0f86edf5f838bfe97b55e2c4892ddbbc815ae904
KHajzner <khajzner@gmail.com>
2023-11-13 13:18:14 +0000

    Fixed mud sliding

--- Gaming Coursework/Assets/Scripts/PlayerMovement.cs
+++ Gaming Coursework/Assets/Scripts/PlayerMovement.cs
@@ -9,13 +9,15 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     bool slides;
     Vector2 direction;
+    Vector2 standardVector;
+    Vector2 standardsVector;
     // Update is called once per frame
     void Update()
     {   
-        if(!slides){
-            direction.x = Input.GetAxisRaw("Horizontal");
-            direction.y = Input.GetAxisRaw("Vertical");
-        }
+        if (!slides){
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+}
     }
 
     void FixedUpdate()
@@ -28,8 +30,17 @@ public class PlayerMovement : MonoBehaviour
             speed = 3f;
         }
 
-        if(collision.tag == "Mud"){
+        if(collision.tag == "MudVertical"){
             speed = 7f;
+            standardVector = new Vector2(0.0f, direction.y);
+            rb.MovePosition(rb.position + standardVector * speed * Time.fixedDeltaTime);
+            slides = true;
+        }
+        
+        if(collision.tag == "MudHorizontal"){
+            speed = 7f;
+            standardVector = new Vector2(direction.x, 0.0f);
+            rb.MovePosition(rb.position + standardVector * speed * Time.fixedDeltaTime);
             slides = true;
         }
     }

0c17e946d592552be09c0a22a98c2b56ab5289b7
KHajzner <khajzner@gmail.com>
2023-11-20 13:56:55 +0000

    Basic lava spawning

--- Gaming Coursework/Assets/Scripts/Lava.cs
+++ Gaming Coursework/Assets/Scripts/Lava.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Lava : MonoBehaviour
 {
+
+    public float delay;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(MoveLava(20, delay));
+        StartCoroutine(MoveLava(-20, delay+20));
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator MoveLava(float moveBy, float delay)
     {
-        
+        yield return new WaitForSeconds(delay);
+        transform.position = new Vector3(moveBy, 0, 0);
     }
-
 }

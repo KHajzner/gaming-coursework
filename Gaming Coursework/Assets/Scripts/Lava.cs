98a1199287e4169eb9fcd47d26dbd72d69d45dae
KHajzner <khajzner@gmail.com>
2023-11-20 14:11:51 +0000

    Improved Lava spawning mechanic

--- Gaming Coursework/Assets/Scripts/Lava.cs
+++ Gaming Coursework/Assets/Scripts/Lava.cs
@@ -7,15 +7,23 @@ public class Lava : MonoBehaviour
 {
 
     public float delay;
-    // Start is called before the first frame update
+    float delayBetween = 0.5f;
+
     void Start()
     {
         StartCoroutine(MoveLava(20, delay));
-        StartCoroutine(MoveLava(-20, delay+20));
+        StartCoroutine(MoveLava(0, delay+50));
     }
     IEnumerator MoveLava(float moveBy, float delay)
     {
         yield return new WaitForSeconds(delay);
         transform.position = new Vector3(moveBy, 0, 0);
+        for (int i = 0; i<5; i++)
+        {
+            yield return new WaitForSeconds(delayBetween);
+            transform.position = new Vector3(0, 0, 0);
+            yield return new WaitForSeconds(delayBetween);
+            transform.position = new Vector3(moveBy, 0, 0);
+        }
     }
 }

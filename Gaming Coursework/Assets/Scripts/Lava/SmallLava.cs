8ee7b2c4bce21f907a35e331b845631b08a114fe
KHajzner <khajzner@gmail.com>
2023-12-02 12:46:49 +0000

    Fixed Lava Behaviour

--- Gaming Coursework/Assets/Scripts/Lava/SmallLava.cs
+++ Gaming Coursework/Assets/Scripts/Lava/SmallLava.cs
@@ -5,26 +5,68 @@ using UnityEngine.Tilemaps;
 
 public class SmallLava : MonoBehaviour
 {
+ public Tilemap lavaArea;
     public Tilemap spawnArea;
-    public Tilemap lavaArea;
-    public Vector3 offset;
+    public Tilemap midLava;
+    public Tilemap bigLava;
+    public float middelay;
+    public float bigdelay;
+        float delayBetween = 0.5f;
     void Start()
     {
-        transform.position = offset;
-        for (int n = lavaArea.cellBounds.xMin; n < lavaArea.cellBounds.xMax; n++)
+        // Get the bounds of the lavaPool Tilemap
+        BoundsInt bounds = lavaArea.cellBounds;
+
+        // Loop through each cell in the lavaPool Tilemap
+        foreach (Vector3Int cellPosition in bounds.allPositionsWithin)
         {
-            for (int p = lavaArea.cellBounds.yMin; p < lavaArea.cellBounds.yMax; p++)
+            // Check if the current cell is a lavaPool tile
+            if (lavaArea.HasTile(cellPosition))
             {
-               Vector3Int localPlace = (new Vector3Int(n, p, (int)lavaArea.transform.position.y));
-                if (lavaArea.HasTile(localPlace))
-                {
-                    Vector3 cellPosition = lavaArea.CellToWorld(localPlace);
-                    var emptyTheCell = spawnArea.WorldToCell(cellPosition);
-                    spawnArea.SetTile(emptyTheCell, null);
-                }
+                // Remove the tile in the spawnArea at the same position
+                Vector3Int spawnAreaPosition = new Vector3Int(cellPosition.x, cellPosition.y, 0);
+                spawnArea.SetTile(spawnAreaPosition, null);
             }
         }
+        midLava.gameObject.SetActive(false);
+        bigLava.gameObject.SetActive(false);
+        StartCoroutine(MoveLava(middelay, midLava));
+        // bigLava.StartMove(bigdelay);
     }
-    
+
+    IEnumerator MoveLava(float delay, Tilemap lavaPool)
+    {
+        Collider2D col = lavaPool.GetComponent<Collider2D>();
+        if (col != null){
+             col.enabled = false;
+        }
+        yield return new WaitForSeconds(delay);
+                lavaPool.gameObject.SetActive(true);
+        for (int i = 0; i<3; i++)
+        {
+            yield return new WaitForSeconds(delayBetween);
+                    lavaPool.gameObject.SetActive(false);
+            yield return new WaitForSeconds(delayBetween);
+                    lavaPool.gameObject.SetActive(true);
+        }
+        // Get the bounds of the lavaPool Tilemap
+        BoundsInt bounds = lavaPool.cellBounds;
+
+        // Loop through each cell in the lavaPool Tilemap
+        foreach (Vector3Int cellPosition in bounds.allPositionsWithin)
+        {
+            // Check if the current cell is a lavaPool tile
+            if (lavaPool.HasTile(cellPosition))
+            {
+                // Remove the tile in the spawnArea at the same position
+                Vector3Int spawnAreaPosition = new Vector3Int(cellPosition.x, cellPosition.y, 0);
+                spawnArea.SetTile(spawnAreaPosition, null);
+            }
+        }
+
+        if (col != null){
+             col.enabled = true;
+        }
+    }
 
 }
---
+++ Gaming Coursework/Assets/Scripts/LavaBehaviour.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class LavaBehaviour : MonoBehaviour
+{
+    public Tilemap smallPool, spawnArea, middlePool, bigPool;
+    public float middleDelay, bigDelay;
+    float delayBetween = 0.5f;
+    // Start is called before the first frame update
+    void Awake(){
+        removeSpawn(smallPool);
+    }
+    void Start()
+    {
+        middlePool.gameObject.SetActive(false);
+        bigPool.gameObject.SetActive(false);
+        StartCoroutine(MoveLava(middleDelay, middlePool));
+        StartCoroutine(MoveLava(bigDelay, bigPool));
+    }
+
+    void removeSpawn(Tilemap lavaPool){
+        foreach (var position in lavaPool.cellBounds.allPositionsWithin){
+            if(lavaPool.HasTile(position)){
+                Vector3Int spawnAreaPosition = new Vector3Int(position.x, position.y, 0);
+                spawnArea.SetTile(spawnAreaPosition, null);
+            }
+        }
+    }
+    IEnumerator MoveLava(float delay, Tilemap lavaPool){
+        Collider2D col = lavaPool.GetComponent<Collider2D>();
+        if (col != null){
+             col.enabled = false;
+        }
+        yield return new WaitForSeconds(delay);
+        lavaPool.gameObject.SetActive(true);
+        for (int i = 0; i<3; i++)
+        {
+            yield return new WaitForSeconds(delayBetween);
+            lavaPool.gameObject.SetActive(false);
+            yield return new WaitForSeconds(delayBetween);
+            lavaPool.gameObject.SetActive(true);
+        }
+        removeSpawn(lavaPool);
+        if(col != null){
+            col.enabled = true;
+        }
+    }
+}
